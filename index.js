b6dca0e6140b5162c1bb48392aaca74be9e69eac
Rory Quinlan <rjamesquinlan@gmail.com>
2018-07-14 15:02:48 -0400

    loads vector source but getting strange quantizeArray error in console

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-import Map from 'ol/Map.js'
-import View from 'ol/View.js'
-import TileLayer from 'ol/layer/Tile.js'
-import VectorSource from 'ol/source/Vector.js'
-import Vector from 'ol/layer/Vector.js'
-import { fromLonLat } from 'ol/proj.js'
-import Stamen from 'ol/source/Stamen.js'
-import GeoJSON from 'ol/format/GeoJSON.js'
-import Select from 'ol/interaction/Select.js'
-import DragBox from 'ol/interaction/DragBox.js'
-import { platformModifierKeyOnly } from 'ol/events/condition.js'
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import TileLayer from 'ol/layer/Tile.js';
+import Vector from 'ol/source/Vector.js';
+import VectorLayer from 'ol/layer/Vector.js';
+import { fromLonLat } from 'ol/proj.js';
+import Stamen from 'ol/source/Stamen.js';
+import GeoJSON from 'ol/format/GeoJSON.js';
+import countiesJsonUrl from './geojson/counties.geojson';
 
+const vectorSource = new Vector({
+  url: countiesJsonUrl,
+  format: new GeoJSON(),
+  overlaps: false,
+});
 
-const vectorSource = new VectorSource({
-  url: 'geojson/counties.json',
-  format: new GeoJSON()
-})
+const vectorLayer = new VectorLayer({
+  source: vectorSource,
+  projection: 'EPSG:3857'
+});
+
 const map = new Map({
   layers: [
     new TileLayer({
@@ -27,56 +31,20 @@ const map = new Map({
         layer: 'toner-labels'
       })
     }),
-    new Vector({
-      source: vectorSource
-    })
+    vectorLayer,
   ],
   target: 'map',
   view: new View({
     center: fromLonLat([-77.4830, 37.5407]),
     zoom: 12
   })
-})
-
-// a normal select interaction to handle click
-var select = new Select();
-map.addInteraction(select);
-
-var selectedFeatures = select.getFeatures();
-
-// a DragBox interaction used to select features by drawing boxes
-var dragBox = new DragBox({
-  condition: platformModifierKeyOnly
 });
-
-map.addInteraction(dragBox);
 
 var infoBox = document.getElementById('info');
 
-dragBox.on('boxend', function () {
-  // features that intersect the box are added to the collection of
-  // selected features, and their names are displayed in the "info"
-  // div
-  var info = [];
-  var extent = dragBox.getGeometry().getExtent();
-  vectorSource.forEachFeatureIntersectingExtent(extent, function (feature) {
-    selectedFeatures.push(feature);
-    info.push(feature.get('name'));
-  });
-  if (info.length > 0) {
-    infoBox.innerHTML = info.join(', ');
-  }
-  console.log(selectedFeatures)
-});
-
-// clear selection when drawing a new box and when clicking on the map
-dragBox.on('boxstart', function () {
-  selectedFeatures.clear();
-  infoBox.innerHTML = '&nbsp;';
-});
 map.on('click', function () {
   console.log(vectorSource.getFeatures())
 
   selectedFeatures.clear();
-  infoBox.innerHTML = '&nbsp;';
+  infoBox.innerHTML = 'Changed!';
 });
